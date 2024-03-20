38624ed486b4823aec8eeb8451275415bdfdec8c
John Källström <johnkaellstrom@gmail.com>
2024-03-20 15:22:27 +0100

    Garage handler

---
+++ GarageManager/Handlers/GarageHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace GarageManager.Handlers
+{
+	internal class GarageHandler : IGarageHandler
+	{
+		private IGarage<IVehicle> _garage;
+
+		public GarageHandler(IGarage<IVehicle> garage)
+		{
+			_garage = garage;
+		}
+
+		public IVehicle[] GetAll() => _garage.Vehicles;
+
+		public void ParkVehicle(IVehicle vehicle)
+		{
+			_garage.Park(vehicle);
+		}
+
+		public void RemoveVehicle(IVehicle vehicle)
+		{
+			_garage.Remove(vehicle);
+		}
+	}
+}
---
+++ GarageManager/Handlers/IGarageHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageManager.Handlers
+{
+	internal interface IGarageHandler
+	{
+		void RemoveVehicle(IVehicle vehicle);
+		void ParkVehicle(IVehicle vehicle);
+		IVehicle[] GetAll();
+	}
+}
