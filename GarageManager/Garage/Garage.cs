4a674c1c7a9a908e817c6f5851553603d17854a0
John Källström <johnkaellstrom@gmail.com>
2024-03-20 15:23:20 +0100

    Garage interface and implementation

--- GarageManager/Garage/Garage.cs
+++ GarageManager/Garage/Garage.cs
@@ -1,6 +1,62 @@
 ﻿namespace GarageManager.Garage
 {
-	internal class Garage : IGarage
+	internal class Garage<T> : IGarage<T> where T : IVehicle
 	{
+        private T[] _vehicles;
+		private int _capacity;
+
+        public Garage(int capacity)
+        {
+            _capacity = capacity;
+            _vehicles = new T[_capacity];
+        }
+
+		public T[] Vehicles => _vehicles;
+
+        public void Park(T vehicle)
+		{
+			if (vehicle is null) throw new ArgumentNullException(nameof(vehicle));
+
+			for (int i = 0; i < _capacity; i++)
+			{
+				IVehicle current = _vehicles[i];
+				if (current is null)
+				{
+					_vehicles[i] = vehicle;
+					break;
+				}
+			}
+		}
+
+		public void Remove(T vehicle)
+		{
+			if (vehicle is null) throw new ArgumentNullException(nameof(vehicle));
+
+			for (int i = 0; i < _capacity; i++)
+			{
+				IVehicle current = _vehicles[i];
+				if (current is not null && current.Equals(vehicle))
+				{
+					_vehicles[i] = default!;
+					break;
+				}
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			foreach (var vehicle in _vehicles)
+			{
+				yield return vehicle;
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			foreach (var vehicle in _vehicles)
+			{
+				yield return vehicle;
+			}
+		}
 	}
 }
--- GarageManager/Garage/IGarage.cs
+++ GarageManager/Garage/IGarage.cs
@@ -1,6 +1,9 @@
 ﻿namespace GarageManager.Garage
 {
-	internal interface IGarage
+	internal interface IGarage<T> : IEnumerable<T> where T : IVehicle
 	{
+        T[] Vehicles { get; }
+        void Park(T vehicle);
+		void Remove(T vehicle);
 	}
 }
