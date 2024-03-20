766f00463d574ee0ac2b5aff79679c5f353c4c91
John Källström <johnkaellstrom@gmail.com>
2024-03-20 15:23:51 +0100

    Usings

--- GarageManager/Usings.cs
+++ GarageManager/Usings.cs
@@ -1,3 +1,7 @@
 ﻿global using GarageManager;
 global using GarageManager.UI;
 global using GarageManager.UI.Constants;
+global using GarageManager.Garage;
+global using GarageManager.Garage.Vehicles;
+
+global using System.Collections;
