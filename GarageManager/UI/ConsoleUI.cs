7638057a7cf388e73092c8d7bd73d56e8b9570bd
John Källström <johnkaellstrom@gmail.com>
2024-03-21 09:25:47 +0100

    UI

--- GarageManager/UI/ConsoleUI.cs
+++ GarageManager/UI/ConsoleUI.cs
@@ -2,6 +2,7 @@
 {
 	internal class ConsoleUI : IUserInterface
 	{
+		public void Space() => Console.WriteLine();
 		public void Clear() => Console.Clear();
 
 		public void Dots()
@@ -13,9 +14,9 @@
 			}
 		}
 
-		public void Print(string message) => Console.WriteLine(message);
+		public void Print(string? message) => Console.WriteLine(message);
 
-		public void Print(string message, bool newLine)
+		public void Print(string? message, bool newLine)
 		{
 			if (!newLine) Console.Write(message);
 			else Console.WriteLine(message);
@@ -46,33 +47,6 @@
 
             Console.WriteLine();
         }
-
-		public int ReadInt(string prompt)
-		{
-			while (true)
-			{
-				Console.Write(prompt);
-				if (int.TryParse(Console.ReadLine(), out int result))
-				{
-					return result;
-				}
-			}
-		}
-
-		public int ReadInt(string prompt, int min, int max)
-		{
-			while (true)
-			{
-				Console.Write(prompt);
-				if (int.TryParse(Console.ReadLine(), out int result))
-				{
-					if (result >= min && result <= max)
-					{
-						return result;
-					}
-				}
-			}
-		}
 
 		public string? ReadString(string prompt)
 		{
--- GarageManager/UI/IUserInterface.cs
+++ GarageManager/UI/IUserInterface.cs
@@ -2,14 +2,13 @@
 {
 	public interface IUserInterface
     {
-        void Print(string message);
-        void Print(string message, bool newLine);
+        void Print(string? message);
+        void Print(string? message, bool newLine);
         void DisplayMenu(string[] options);
         void DisplayMenu(string[] options, string title);
         void Clear();
         string? ReadString(string prompt);
-        int ReadInt(string prompt);
-        int ReadInt(string prompt, int min, int max);
         void Dots();
+        void Space();
     }
 }
