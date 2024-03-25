ec81484f5e7bbc3a962c0c4e016713f0078af008
John Källström <johnkaellstrom@gmail.com>
2024-03-25 08:00:40 +0100

    Add vehicles

---
+++ GarageManager/Garage/Vehicles/Spaceship.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageManager.Garage.Vehicles
+{
+	internal class Spaceship : Vehicle
+	{
+		public Spaceship(string registrationNumber, string color, int numberOfWheels) : base(registrationNumber, color, numberOfWheels)
+		{
+		}
+	}
+}
