ab94948f129fba7fbe038eaf8a1e2a72ae60742f
John Källström <johnkaellstrom@gmail.com>
2024-03-25 08:58:39 +0100

    Add more subclasses

---
+++ GarageManager/Garage/Vehicles/Airplane.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageManager.Garage.Vehicles
+{
+	internal class Airplane : Vehicle
+	{
+		public Airplane(string registrationNumber, string color, int numberOfWheels) : base(registrationNumber, color, numberOfWheels)
+		{
+		}
+	}
+}
---
+++ GarageManager/Garage/Vehicles/Bus.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageManager.Garage.Vehicles
+{
+	internal class Bus : Vehicle
+	{
+		public Bus(string registrationNumber, string color, int numberOfWheels) : base(registrationNumber, color, numberOfWheels)
+		{
+		}
+	}
+}
