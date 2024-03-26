5b94a3a789d00ace14397624776e2cf9c375c5b9
John Källström <johnkaellstrom@gmail.com>
2024-03-26 10:04:29 +0100

    Moved reding input to static classs

--- GarageManager/UI/IUserInterface.cs
+++ GarageManager/UI/IUserInterface.cs
@@ -7,10 +7,6 @@
         void PrintMessageWithDots(string? message);
         void PrintSubMenu(string[] options);
         void Clear();
-        (bool IsValid, IVehicle Vehicle) ReadVehicleData(VehicleType type);
-        (bool IsValid, string Value) ReadString(string prompt);
-        (bool IsValid, int Value) ReadInt(string prompt);
-        (bool IsValid, int Value) ReadInt(string prompt, int min, int max);
         void Space();
     }
 }
---
+++ GarageManager/UI/InputReader.cs
@@ -0,0 +1,40 @@
+﻿namespace GarageManager.UI
+{
+	internal static class InputReader
+	{
+		public static (bool IsValid, string Value) GetString(string prompt)
+		{
+			bool valid = false;
+			string str = string.Empty;
+
+			Console.Write(prompt);
+			str = Console.ReadLine()!;
+
+			if (!string.IsNullOrWhiteSpace(str))
+			{
+				valid = true;
+			}
+
+			return (valid, str);
+		}
+
+		public static (bool IsValid, int Value) GetInt(string prompt, int min, int max)
+		{
+			bool valid = false;
+			int num = 0;
+
+			Console.Write(prompt);
+			string? input = Console.ReadLine();
+			if (int.TryParse(input, out int result))
+			{
+				if (result >= min && result <= max)
+				{
+					valid = true;
+					num = result;
+				}
+			}
+
+			return (valid, num);
+		}
+	}
+}
